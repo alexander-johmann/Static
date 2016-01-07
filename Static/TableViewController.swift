16d30dd152b9f5bb8b99f2c58a8ad9ea869eeb3a
Alexander Johmann <aj@whateverworks.at>
2016-01-07 23:19:51 +0100

    TableViewController updated to use UITableViewController

--- Static/TableViewController.swift
+++ Static/TableViewController.swift
@@ -1,20 +1,9 @@
 import UIKit
 
 /// Table view controller with a `DataSource` setup to use its `tableView`.
-public class TableViewController: UIViewController {
+public class TableViewController: UITableViewController {
 
     // MARK: - Properties
-
-    /// Set UITableViewStyle in IB
-    @IBInspectable var grouped: Bool = false
-
-    /// Returns the table view managed by the controller object.
-    public var tableView: UITableView!
-
-    /// A Boolean value indicating if the controller clears the selection when the table appears.
-    ///
-    /// The default value of this property is YES. When YES, the table view controller clears the table’s current selection when it receives a viewWillAppear: message. Setting this property to NO preserves the selection.
-    public var clearsSelectionOnViewWillAppear: Bool = true
 
     /// Table view data source.
     public var dataSource = DataSource() {
@@ -28,69 +17,11 @@ public class TableViewController: UIViewController {
     }
 
 
-    // MARK: - Initialization
-
-    public init(style: UITableViewStyle) {
-        tableView = UITableView(frame: .zero, style: style)
-        super.init(nibName: nil, bundle: nil)
-        dataSource.tableView = tableView
-    }
-
-    public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
-        tableView = UITableView(frame: .zero, style: .Plain)
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-        dataSource.tableView = tableView
-    }
-
-    public required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
-
-    public convenience init() {
-        self.init(style: .Plain)
-    }
-
-
     // MARK: - UIViewController
 
-    public override func loadView() {
-        if tableView == nil {
-            let style: UITableViewStyle = grouped ? .Grouped : .Plain
-            tableView = UITableView(frame: .zero, style: style)
-            dataSource.tableView = tableView
-        }
-        tableView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
-        view = tableView
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        dataSource.tableView = tableView
     }
 
-    public override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        performInitialLoad()
-        clearSelectionsIfNecessary()
-    }
-
-    public override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-        tableView.flashScrollIndicators()
-    }
-
-    public override func setEditing(editing: Bool, animated: Bool) {
-        super.setEditing(editing, animated: animated)
-        tableView.setEditing(editing, animated: animated)
-    }
-
-
-    // MARK: - Private
-
-    private var initialLoadOnceToken = dispatch_once_t()
-    private func performInitialLoad() {
-        dispatch_once(&initialLoadOnceToken) {
-            self.tableView.reloadData()
-        }
-    }
-
-    private func clearSelectionsIfNecessary() {
-        guard clearsSelectionOnViewWillAppear else { return }
-        tableView.indexPathsForSelectedRows?.forEach { tableView.deselectRowAtIndexPath($0, animated: true) }
-    }
 }
